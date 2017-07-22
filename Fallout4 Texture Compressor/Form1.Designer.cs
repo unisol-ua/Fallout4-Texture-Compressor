83e8f2ca60b6977377b2e37138b002a253f916f3
Admirativ <admirativ.com@gmail.com>
2017-07-22 15:48:17 +0300

    inital

---
+++ Fallout4 Texture Compressor/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Fallout4_Texture_Compressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(270, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(140, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(457, 303);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(94, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(503, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(13, 64);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Compress to BC1";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "if > 4",
+            "if > 8",
+            "if > 16",
+            "if > 32",
+            "if > 64",
+            "if > 128",
+            "if > 256",
+            "if > 512",
+            "if > 1024",
+            "if > 2048",
+            "if > 4096",
+            "if > 8192"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 136);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(108, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "if > 2048";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 87);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Resize down";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 26);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Reduces texture dividing\r\nby 2 width and height";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(12, 198);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(93, 17);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.Text = "Make Backup";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 26);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Compares to the\r\nbiggest of those two";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 355);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 381);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Texture Compressor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Fallout4 Texture Compressor/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Fallout4_Texture_Compressor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("settings.xml"))
+            {
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load("settings.xml");
+                XmlElement xRoot = xDoc.DocumentElement;
+                foreach (XmlNode xnode in xRoot)
+                {
+                    if (xnode.Name == "lastdir")
+                    {
+                        textBox1.Text = xnode.InnerText;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double filessize = 0;
+            double newfilessize = 0;
+            Form1 form = this;
+            listBox1.Items.Clear();
+            string path = textBox1.Text;
+            double i = 0;
+            string[] allfiles = Directory.GetFiles(path, "*.dds", SearchOption.AllDirectories);
+            if (checkBox3.Checked == true) // backup
+            {
+                foreach (string file in allfiles)
+                {
+                    i++;
+                    form.Text = "Copying files: " + i + " of " + allfiles.Length + " : " + Math.Round(i / ((Double)allfiles.Length / 100), 2) + "%";
+                    FileInfo fileinf = new FileInfo(file);
+                    string newfolders = fileinf.DirectoryName.Replace(textBox1.Text, "");
+                    Directory.CreateDirectory(Application.StartupPath + "\\Backup" + newfolders);
+                    File.Copy(file, Application.StartupPath + "\\Backup" + fileinf.FullName.Replace(textBox1.Text, ""), true);
+                }
+                form.Text = "Archiving files";
+                string time = DateTime.Now.Second + "s_" + DateTime.Now.Minute + "m_" + DateTime.Now.Hour + "h_" + DateTime.Now.Day + "d_" + DateTime.Now.Month + "m_" + DateTime.Now.Year + "y";
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.RedirectStandardError = true;
+                startInfo.FileName = Application.StartupPath + "\\bin\\7za.exe";
+                startInfo.Arguments = "a backup_" + time + ".zip \"" + Application.StartupPath + "\\Backup\\*\" -mx6 -o" + Application.StartupPath + "\\";
+                process.StartInfo = startInfo;
+                process.Start();
+                process.WaitForExit();
+                Directory.Delete(Application.StartupPath + "\\Backup", true);
+            }
+            i = 0;
+            foreach (string file in allfiles)
+            {
+                FileInfo fileinf = new FileInfo(file);
+                listBox1.Items.Add(file);
+                fileinfo ddsinfo = checkdds(file);
+                double filesize = Math.Round((Double)new FileInfo(file).Length / 1024, 1);
+                filessize += filesize;
+                listBox1.Items.Add("file size = " + filesize + " kb");
+                int size = 0;
+                if (ddsinfo.height > ddsinfo.width) { size = ddsinfo.height; }
+                else { size = ddsinfo.width; }
+                //checkBox1 - compress
+                //checkBox2 - resize
+                if (checkBox1.Checked == true && checkBox2.Checked == true) // compress + resize
+                {
+                    i++;
+                    form.Text = "Compressing and resizing files : " + i + " of " + allfiles.Length + " : " + Math.Round(i / ((Double)allfiles.Length / 100), 2) + "%";
+                    int ifgreater = 8192;
+                    if (comboBox1.Text.Contains("512")) { ifgreater = 512; }
+                    else if (comboBox1.Text.Contains("1024")) { ifgreater = 1024; }
+                    else if (comboBox1.Text.Contains("2048")) { ifgreater = 2048; }
+                    else if (comboBox1.Text.Contains("4096")) { ifgreater = 4096; }
+                    else if (comboBox1.Text.Contains("128")) { ifgreater = 128; }
+                    else if (comboBox1.Text.Contains("256")) { ifgreater = 256; }
+                    else if (comboBox1.Text.Contains("8192")) { ifgreater = 8192; }
+                    else if (comboBox1.Text.Contains("4")) { ifgreater = 4; }
+                    else if (comboBox1.Text.Contains("8")) { ifgreater = 8; }
+                    else if (comboBox1.Text.Contains("16")) { ifgreater = 16; }
+                    else if (comboBox1.Text.Contains("32")) { ifgreater = 32; }
+                    else if (comboBox1.Text.Contains("64")) { ifgreater = 64; }
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.CreateNoWindow = true;
+                    startInfo.UseShellExecute = false;
+                    startInfo.FileName = Application.StartupPath + "\\bin\\texconv.exe";
+                    bool needtogenmm = false;
+                    if (size > ifgreater)
+                    {
+                        if (ddsinfo.format.Contains("SRGB"))
+                        {
+                            startInfo.Arguments = "\"" + file + "\" -y -f BC1_UNORM_SRGB -o \"" + fileinf.DirectoryName + "\" -w " + ddsinfo.width / 2 + " -h " + ddsinfo.height / 2 + " -m 1";
+                            listBox1.Items.Add("new  width = " + (ddsinfo.width / 2));
+                            listBox1.Items.Add("new height = " + (ddsinfo.height / 2));
+                            listBox1.Items.Add("new format = BC1_UNORM_SRGB");
+                        }
+                        else
+                        {
+                            startInfo.Arguments = "\"" + file + "\" -y -f BC1_UNORM -o \"" + fileinf.DirectoryName + "\" -w " + ddsinfo.width / 2 + " -h " + ddsinfo.height / 2 + " -m 1";
+                            listBox1.Items.Add("new  width = " + (ddsinfo.width / 2));
+                            listBox1.Items.Add("new height = " + (ddsinfo.height / 2));
+                            listBox1.Items.Add("new format = BC1_UNORM");
+                        }
+                        needtogenmm = true;
+                    }
+                    else
+                    {
+                        if (ddsinfo.format.Contains("SRGB"))
+                        {
+                            startInfo.Arguments = "\"" + file + "\" -y -f BC1_UNORM_SRGB -o \"" + fileinf.DirectoryName + "\"";
+                            listBox1.Items.Add("new format = BC1_UNORM_SRGB");
+                        }
+                        else
+                        {
+                            startInfo.Arguments = "\"" + file + "\" -y -f BC1_UNORM -o \"" + fileinf.DirectoryName + "\"";
+                            listBox1.Items.Add("new format = BC1_UNORM");
+                        }
+                    }
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.WaitForExit();
+                    if (needtogenmm)
+                    {
+                        //gen mipmaps
+                        startInfo.Arguments = "\"" + file + "\" -y -o \"" + fileinf.DirectoryName + "\" -w " + ddsinfo.width / 2 + " -h " + ddsinfo.height / 2 + " -m 0";
+                        process.StartInfo = startInfo;
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                }
+                else if (checkBox1.Checked == true) //compress
+                {
+                    i++;
+                    form.Text = "Compressing files : " + i + " of " + allfiles.Length + " : " + Math.Round(i / ((Double)allfiles.Length / 100), 2) + "%";
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.CreateNoWindow = true;
+                    startInfo.UseShellExecute = false;
+                    startInfo.FileName = Application.StartupPath + "\\bin\\texconv.exe";
+                    if (ddsinfo.format.Contains("SRGB"))
+                    {
+                        startInfo.Arguments = "\"" + file + "\" -y -f BC1_UNORM_SRGB -o \"" + fileinf.DirectoryName + "\"";
+                        listBox1.Items.Add("new format = BC1_UNORM_SRGB");
+                    }
+                    else
+                    {
+                        startInfo.Arguments = "\"" + file + "\" -y -f BC1_UNORM -o \"" + fileinf.DirectoryName + "\"";
+                        listBox1.Items.Add("new format = BC1_UNORM");
+                    }
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.WaitForExit();
+                }
+                else if (checkBox2.Checked == true) //resize
+                {
+                    i++;
+                    form.Text = "Resizing files : " + i + " of " + allfiles.Length + " : " + Math.Round(i / ((Double)allfiles.Length / 100), 2) + "%";
+                    int ifgreater = 8192;
+                    if (comboBox1.Text.Contains("512")) { ifgreater = 512; }
+                    else if (comboBox1.Text.Contains("1024")) { ifgreater = 1024; }
+                    else if (comboBox1.Text.Contains("2048")) { ifgreater = 2048; }
+                    else if (comboBox1.Text.Contains("4096")) { ifgreater = 4096; }
+                    else if (comboBox1.Text.Contains("128")) { ifgreater = 128; }
+                    else if (comboBox1.Text.Contains("256")) { ifgreater = 256; }
+                    else if (comboBox1.Text.Contains("8192")) { ifgreater = 8192; }
+                    else if (comboBox1.Text.Contains("4")) { ifgreater = 4; }
+                    else if (comboBox1.Text.Contains("8")) { ifgreater = 8; }
+                    else if (comboBox1.Text.Contains("16")) { ifgreater = 16; }
+                    else if (comboBox1.Text.Contains("32")) { ifgreater = 32; }
+                    else if (comboBox1.Text.Contains("64")) { ifgreater = 64; }
+                    if (size > ifgreater)
+                    {
+                        Process process = new Process();
+                        ProcessStartInfo startInfo = new ProcessStartInfo();
+                        startInfo.CreateNoWindow = true;
+                        startInfo.UseShellExecute = false;
+                        startInfo.FileName = Application.StartupPath + "\\bin\\texconv.exe";
+                        startInfo.Arguments = "\"" + file + "\" -y -o \"" + fileinf.DirectoryName + "\" -w " + ddsinfo.width / 2 + " -h " + ddsinfo.height / 2 + " -m 1";
+                        process.StartInfo = startInfo;
+                        process.Start();
+                        process.WaitForExit();
+                        //gen mipmaps
+                        startInfo.Arguments = "\"" + file + "\" -y -o \"" + fileinf.DirectoryName + "\" -w " + ddsinfo.width / 2 + " -h " + ddsinfo.height / 2 + " -m 0";
+                        process.StartInfo = startInfo;
+                        process.Start();
+                        process.WaitForExit();
+                        listBox1.Items.Add("new  width = " + (ddsinfo.width / 2));
+                        listBox1.Items.Add("new height = " + (ddsinfo.height / 2));
+                    }
+                }
+                filesize = Math.Round((Double)new FileInfo(file).Length / 1024, 1);
+                newfilessize += filesize;
+                listBox1.Items.Add("new file size = " + filesize + " kb");
+            }
+            form.Text = "Compressed from " + Math.Round(filessize / 1024, 3) + "mb to " + Math.Round(newfilessize / 1024, 3) + "mb Saved = " + Math.Round((filessize - newfilessize)/1024, 3) + "mb";
+        }
+
+        private fileinfo checkdds(string file)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.FileName = Application.StartupPath + "\\bin\\texdiag.exe";
+            startInfo.Arguments = "info \"" + file + "\"";
+            process.StartInfo = startInfo;
+            process.Start();
+            fileinfo ddsinfo = new fileinfo();
+            while (!process.StandardOutput.EndOfStream)
+            {
+                string line = process.StandardOutput.ReadLine();
+                if (line.Contains("height"))
+                {
+                    ddsinfo.height = int.Parse(line.Substring(line.IndexOf("=") + 2, line.Length - line.IndexOf("=") - 2));
+                }
+                if (line.Contains("width"))
+                {
+                    ddsinfo.width = int.Parse(line.Substring(line.IndexOf("=") + 2, line.Length - line.IndexOf("=") - 2));
+                }
+                if (line.Contains("format"))
+                {
+                    ddsinfo.format = line.Substring(line.IndexOf("=") + 2, line.Length - line.IndexOf("=") - 2);
+                }
+            }
+            process.WaitForExit();
+            listBox1.Items.Add("height = " + ddsinfo.height);
+            listBox1.Items.Add(" width = " + ddsinfo.width);
+            listBox1.Items.Add("format = " + ddsinfo.format);
+            return ddsinfo;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Description = "Choose textures folder";
+            folderBrowserDialog1.SelectedPath = textBox1.Text; ;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                XDocument xdoc = new XDocument();
+                XElement settings = new XElement("settings");
+                XElement dir = new XElement("lastdir", textBox1.Text);
+                settings.Add(dir);
+                xdoc.Add(settings);
+                xdoc.Save("settings.xml");
+            }
+        }
+    }
+
+    public class fileinfo
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string format { get; set; }
+    }
+}
